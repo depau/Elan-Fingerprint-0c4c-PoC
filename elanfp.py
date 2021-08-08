8063ffd3e95595f2ebb29f2c49789b670d36db87
Davide Depau <davide@depau.eu>
2021-08-08 20:37:13 +0200

    Update PoC

--- elanfp.py
+++ elanfp.py
@@ -10,7 +10,9 @@ Usage:
     ARGV0 finger_info <id>
     ARGV0 verify
     ARGV0 enrolled_count
-    ARGV0 enroll
+    ARGV0 enroll (-u UD)
+    ARGV0 delete <id>
+    ARGV0 finger_info_all
     ARGV0 delete_all
     ARGV0 fw_ver
     ARGV0 raw (-e EP) <hex>...
@@ -18,6 +20,7 @@ Usage:
 Options:
 -h, --help         Show help
 -e EP, --ep-in EP  Input endpoint for raw commands
+-u UD. --user UD   User data for enroll command
 
 Commands:
 reset              Reset sensor
@@ -25,6 +28,7 @@ finger_info <id>   Get finger info
 verify             Verify finger
 enrolled_count     Get number of fingers currently enrolled
 enroll             Enroll a new finger
+delete <id>        Delete finger
 delete_all         Delete all enrolled fingers
 fw_ver             Get firmware version
 raw                Send raw command
@@ -32,13 +36,12 @@ raw                Send raw command
 import struct
 import sys
 import warnings
+from collections import namedtuple
 from typing import Optional
 
 import hexdump
 import usb1
 from docopt import docopt
-
-from collections import namedtuple
 
 Command = namedtuple("Command", ("command", "out_len", "in_len", "ep_out", "ep_in"))
 
@@ -49,10 +52,10 @@ COMMANDS = {
     "enrolled_num":  Command(b"\xff\x04", 3, 2, 1, 3),
     "enrolled_num1": Command(b"\xff\x00", 3, 2, 1, 3),
     "abort":         Command(b"\xff\x02", 3, 2, 1, 3),
-    "commit":        Command(b"\xff\x11", 73, 2, 1, 3),
-    "enroll":        Command(b"\xff\x01", 8, 2, 1, 4),
+    "commit":        Command(b"\xff\x11", 72, 2, 1, 3),
+    "enroll":        Command(b"\xff\x01", 7, 2, 1, 4),
     "after_enroll":  Command(b"\xff\x10", 3, 3, 1, 3),
-    "delete":        Command(b"\xff\x13", 73, 2, 1, 3),
+    "delete":        Command(b"\xff\x13", 72, 2, 1, 3),
 }
 
 ERRORS = {
@@ -63,6 +66,7 @@ ERRORS = {
     0xfb: "Sensor is dirty or wet",
     0xfd: "Finger not enrolled",
     0xfe: "Finger area not enough",
+    0xdd: "Maximum number of enrolled fingers reached"
 }
 
 ID_VENDOR = 0x04f3
@@ -70,7 +74,7 @@ ID_PRODUCT = 0x0c4c
 IFACE = 0
 
 
-def command(usb: usb1.USBDeviceHandle, cmdname: str, payload: bytes = b"", timeout=1000) -> bytes:
+def command(usb: usb1.USBDeviceHandle, cmdname: str, payload: bytes = b"", timeout=2000) -> bytes:
     outpayload, outlen, inlen, ep_out, ep_in = COMMANDS[cmdname]
     cmd = b"\x40" + outpayload + payload
     if len(cmd) != outlen:
@@ -94,7 +98,7 @@ def get_error(byte: int) -> Optional[str]:
     return ERRORS[byte]
 
 
-def enroll(handle: usb1.USBDeviceHandle):
+def enroll(handle: usb1.USBDeviceHandle, user_data: bytes):
     resp = command(handle, "enrolled_num")
     error = get_error(resp[1])
     if error:
@@ -116,7 +120,7 @@ def enroll(handle: usb1.USBDeviceHandle):
         print("Proceeding")
         break
 
-    total_attempts = 3
+    total_attempts = 8
     attempts_done = 0
     while attempts_done < total_attempts:
         print(f"Place finger on reader [{attempts_done + 1}/{total_attempts}]")
@@ -125,6 +129,8 @@ def enroll(handle: usb1.USBDeviceHandle):
         error = get_error(resp[1])
         if resp[1] != 0:
             print(f"Error: {error} ({hex(resp[1])})")
+            if resp[1] == 0xdd:
+                return  # Max fingers
             continue
         attempts_done += 1
 
@@ -132,7 +138,7 @@ def enroll(handle: usb1.USBDeviceHandle):
     print(f"Whatever this means: {resp.hex(' ')}")
 
     print("Committing enrolled finger")
-    payload = (struct.pack("B", 0xf0 | (new_finger_id + 5)) + b"I use arch btw").ljust(69, b"\x00")
+    payload = (struct.pack("B", 0xf0 | (new_finger_id + 5)) + user_data).ljust(69, b"\x00")
     resp = command(handle, "commit", payload)
     if resp[1] == 0:
         print("Enroll successful 🎉")
@@ -140,8 +146,45 @@ def enroll(handle: usb1.USBDeviceHandle):
         print(f"Sensor is angry: {resp.hex(' ')}")
 
 
-def delete(usb: usb1.USBDeviceHandle, fpid: int):
-    raise NotImplemented
+def verify(handle: usb1.USBDeviceHandle) -> int:
+    while True:
+        print("Place finger on reader")
+        resp = command(handle, "verify", timeout=5000)
+        error = get_error(resp[1])
+        if error:
+            print(error)
+            continue
+        print(f"Recognized finger: {resp[1]}")
+        return resp[1]
+
+
+def get_finger_info(handle: usb1.USBDeviceHandle, finger_id: int) -> bytes:
+    while True:
+        resp = command(handle, "finger_info", finger_id.to_bytes(1, "little"))
+        if resp[1] == 0xff:
+            print("Sensor is angry, verify a finger to calm it down")
+            verify(handle)
+            continue
+        if len(resp) == 2:
+            raise IOError(f"Error: {get_error(resp[1])}")
+        return resp
+
+
+def delete_by_id(handle: usb1.USBDeviceHandle, fpid: int):
+    resp = get_finger_info(handle, fpid)
+    payload = (struct.pack("B", 0xf0 | (fpid + 5)) + resp[2:]).ljust(69, b"\x00")
+    delete(handle, fpid, payload)
+
+
+def delete(handle: usb1.USBDeviceHandle, fpid: int, payload: bytes):
+    resp = command(handle, "delete", payload)
+    error = get_error(resp[1])
+    if resp[1] != 0:
+        print(f"Error: {error} ({hex(resp[1])})")
+    else:
+        print("Deleted, finger info:")
+        resp = get_finger_info(handle, fpid)
+        hexdump.hexdump(resp)
 
 
 def main(args):
@@ -156,15 +199,7 @@ def main(args):
                     handle.resetDevice()
 
                 elif args["verify"]:
-                    while True:
-                        print("Place finger on reader")
-                        resp = command(handle, "verify", timeout=5000)
-                        error = get_error(resp[1])
-                        if error:
-                            print(error)
-                            continue
-                        print(f"Recognized finger: {resp[1]}")
-                        # break
+                    verify(handle)
 
                 elif args["fw_ver"]:
                     resp = command(handle, "fw_ver")
@@ -172,19 +207,22 @@ def main(args):
 
                 elif args["finger_info"]:
                     finger_id = int(args["<id>"])
-                    resp = command(handle, "finger_info", finger_id.to_bytes(1, "little"))
-                    if len(resp) == 2:
-                        print(f"Error: {get_error(resp[1])}")
-                        return
+                    resp = get_finger_info(handle, finger_id)
                     print("Finger info:")
                     hexdump.hexdump(resp)
+
+                elif args["finger_info_all"]:
+                    for finger_id in range(10):
+                        resp = get_finger_info(handle, finger_id)
+                        print(f"Finger info {finger_id}:")
+                        hexdump.hexdump(resp)
 
                 elif args["enrolled_count"]:
                     resp = command(handle, "enrolled_num")
                     print(f"Enrolled fingers: {resp[1]}")
 
                 elif args["enroll"]:
-                    enroll(handle)
+                    enroll(handle, args["--user"].encode())
 
                 elif args["raw"]:
                     ep = int(args["--ep-in"])
@@ -197,6 +235,18 @@ def main(args):
                     print(f"Received [{len(resp)}]:")
                     hexdump.hexdump(resp)
 
+                elif args["delete"]:
+                    finger_id = int(args["<id>"])
+                    delete_by_id(handle, finger_id)
+
+                elif args["delete_all"]:
+                    for finger_id in range(10):
+                        resp = get_finger_info(handle, finger_id)
+                        if resp[-1] == 0xff:
+                            print(f"Finger {finger_id} not enrolled")
+                            continue
+                        payload = (struct.pack("B", 0xf0 | (finger_id + 5)) + resp[2:]).ljust(69, b"\x00")
+                        delete(handle, finger_id, payload)
 
             except Exception:
                 print("Aborting")
