121bd0eac454fb90ee20fec7dd71e691cd09f45a
Davide Depau <davide@depau.eu>
2024-04-04 23:23:46 +0200

    Add newly discovered commands

--- elanfp.py
+++ elanfp.py
@@ -7,7 +7,8 @@ ELAN 04F3:0C4C Match-on-Chip fingerprint reader driver PoC.
 Usage:
     ARGV0 -h | --help
     ARGV0 info
-    ARGV0 reset
+    ARGV0 soft_reset
+    ARGV0 hard_reset
     ARGV0 finger_info <id>
     ARGV0 verify
     ARGV0 enrolled_count
@@ -17,6 +18,8 @@ Usage:
     ARGV0 delete_all
     ARGV0 wipe_all
     ARGV0 fw_ver
+    ARGV0 read_reg <reg>
+    ARGV0 dump_regs
     ARGV0 raw (-e EP) <hex>...
 
 Options:
@@ -26,7 +29,8 @@ Options:
 
 Commands:
 info               Get device info
-reset              Reset sensor
+soft_reset         Reset sensor (via libusb)
+hard_reset         Reset sensor (via hardware - disconnect and reconnect)
 finger_info <id>   Get finger info
 verify             Verify finger
 enrolled_count     Get number of fingers currently enrolled
@@ -35,6 +39,8 @@ delete <id>        Delete finger
 delete_all         Delete all enrolled fingers (one by one)
 wipe_all           Wipe all enrolled fingers (using special command)
 fw_ver             Get firmware version
+read_reg <reg>     Read register
+dump_regs          Read all registers
 raw                Send raw command
 """
 import struct
@@ -50,18 +56,42 @@ from docopt import docopt
 Command = namedtuple("Command", ("command", "out_len", "in_len", "ep_out", "ep_in"))
 
 COMMANDS = {
-    "fw_ver":                    Command(b"\x19", 2, 2, 1, 3),
-    "verify":                    Command(b"\xff\x03", 3, 2, 1, 4),
-    "finger_info":               Command(b"\xff\x12", 4, 64, 1, 3),
-    "enrolled_num":              Command(b"\xff\x04", 3, 2, 1, 3),
-    "enrolled_num1":             Command(b"\xff\x00", 3, 2, 1, 3),
-    "abort":                     Command(b"\xff\x02", 3, 2, 1, 3),
-    "commit":                    Command(b"\xff\x11", 72, 2, 1, 3),
-    "enroll":                    Command(b"\xff\x01", 7, 2, 1, 4),
-    "check_enrolled_collision":  Command(b"\xff\x10", 3, 3, 1, 3),
-    "delete":                    Command(b"\xff\x13", 72, 2, 1, 3),
-    "wipe_all":                  Command(b"\xff\x99", 3, 0, 1, None),
+    "reset_device": Command(b"'WDTRST", 8, 0, 1, None),
+    "fw_ver": Command(b"\x19", 2, 2, 1, 3),
+    "verify": Command(b"\xff\x03", 3, 2, 1, 4),
+    "finger_info": Command(b"\xff\x12", 4, 70, 1, 3),
+    "enrolled_num": Command(b"\xff\x04", 3, 2, 1, 3),
+    "enrolled_num1": Command(b"\xff\x00", 3, 2, 1, 3),
+    "abort": Command(b"\xff\x02", 3, 2, 1, 3),
+    "commit": Command(b"\xff\x11", 72, 2, 1, 3),
+    "enroll": Command(b"\xff\x01", 7, 2, 1, 4),
+    "check_enrolled_collision": Command(b"\xff\x10", 3, 3, 1, 3),
+    "delete_subsid": Command(b"\xff\x13", 72, 2, 1, 3),
+    "delete": Command(b"\xff\x05", 5, 2, 1, 3),
+    "wipe_all": Command(b"\xff\x99", 3, 0, 1, None),
 }
+
+# --- @'WDTRST
+# 00 09
+# 40  FF 05 xx yy
+# 02 \n
+# 01 \n
+
+# --- 40 40+X    # read register X                         # resp: 1  # 64 registers
+# 40 80+X Y  # write register X with value Y
+# 42 01 52 55 4E 49 41 50   # switch to bootloader
+# 01 0A      # get raw (14 bit) image?
+# 40 19      # get bridge firmware version             # resp: 2
+# 00 C0      # get sensor trace                        # resp: 4
+# 40 13      # get sensor status                       # resp: 1
+# 00 09      # capture start (get image)
+# 40 FF 05 XX YY  # remove finger?                     # resp: 2
+# 02 0A      # get 8bit img?  (rotate if w=52 h=150)
+# 40 ff 14 XX  # set fw sensor mode                    # resp: 4  0 = normal WBF mode, 1 = VBS WBF mode
+# 00 10 0/1  # set EC pin state
+
+# 40 FF 0A ?? (35 bytes)  # ecc enroll committed??     # resp: 2
+
 
 ERRORS = {
     0x41: "Move slightly downwards",
@@ -82,7 +112,7 @@ DEVICES = (
 IFACE = 0
 
 
-def command(usb: usb1.USBDeviceHandle, cmdname: str, payload: bytes = b"", timeout=2000) -> bytes:
+def command(usb: usb1.USBDeviceHandle, cmdname: str, payload: bytes = b"", timeout=5000) -> bytes:
     outpayload, outlen, inlen, ep_out, ep_in = COMMANDS[cmdname]
     cmd = b"\x40" + outpayload + payload
     if len(cmd) != outlen:
@@ -99,6 +129,24 @@ def command(usb: usb1.USBDeviceHandle, cmdname: str, payload: bytes = b"", timeo
         warnings.warn(f"Device replied with shorter answer: {len(cmd)} vs {inlen}")
 
     return resp
+
+
+def read_register(usb: usb1.USBDeviceHandle, reg: int) -> int:
+    if not 0 <= reg < 64:
+        raise ValueError("Register out of range (0-63)")
+    cmd = b"\x40" + bytes([0x40 + reg])
+
+    usb.bulkWrite(1, cmd)
+    resp = usb.bulkRead(3, 2)
+
+    if len(resp) != 2:
+        warnings.warn(f"Device replied with wrong size: {len(resp)}")
+
+    error = get_error(resp[1])
+    if error:
+        raise IOError(f"Failing to read register {reg}: {error}")
+
+    return resp[0]
 
 
 def get_error(byte: int) -> Optional[str]:
@@ -186,13 +234,19 @@ def get_finger_info(handle: usb1.USBDeviceHandle, finger_id: int) -> bytes:
 
 
 def delete_by_id(handle: usb1.USBDeviceHandle, fpid: int):
-    resp = get_finger_info(handle, fpid)
-    payload = (struct.pack("B", 0xf0 | (fpid + 5)) + resp[2:]).ljust(69, b"\x00")
-    delete(handle, fpid, payload)
-
-
-def delete(handle: usb1.USBDeviceHandle, fpid: int, payload: bytes):
+    payload = bytes([fpid, 0])
     resp = command(handle, "delete", payload)
+    error = get_error(resp[1])
+    if resp[1] != 0:
+        print(f"Error: {error} ({hex(resp[1])})")
+    else:
+        print("Deleted, finger info:")
+        resp = get_finger_info(handle, fpid)
+        hexdump.hexdump(resp)
+
+
+def delete(handle: usb1.USBDeviceHandle, fpid: int, finger_sid: bytes):
+    resp = command(handle, "delete_subsid", finger_sid)
     error = get_error(resp[1])
     if resp[1] != 0:
         print(f"Error: {error} ({hex(resp[1])})")
@@ -213,8 +267,12 @@ def main(args):
 
         with handle.claimInterface(IFACE):
             try:
-                if args["reset"]:
+                if args["soft_reset"]:
                     handle.resetDevice()
+
+                elif args["hard_reset"]:
+                    command(handle, "reset_device")
+                    print("Device reset.")
 
                 elif args["info"]:
                     dev: usb1.USBDevice = handle.getDevice()
@@ -285,6 +343,19 @@ def main(args):
                     print("Checking if all fingers are wiped (~5 seconds)")
                     resp = command(handle, "enrolled_num", timeout=10000)
                     print(f"Enrolled fingers: {resp[1]}")
+
+                elif args["read_reg"]:
+                    reg = int(args["<reg>"])
+                    print(f"Register {reg}: {read_register(handle, reg):#02x}")
+
+                elif args["dump_regs"]:
+                    # Print a 8x8 table of registers
+                    print("     x0  x1  x2  x3  x4  x5  x6  x7\n")
+                    for i in range(8):
+                        print(f"{i}x ", end="")
+                        for j in range(8):
+                            print(f"  {read_register(handle, i * 8 + j) :02x}", end="")
+                        print()
 
             except (Exception, KeyboardInterrupt) as e:
                 print("Aborting")
